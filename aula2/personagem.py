357e2278e86fd72971f38affaa6c2778f6e1afa6
pedroVtor <alanzokapq123@gmail.com>
2026-08-20 02:54:28 -0300

    Update personagem.py

--- aula2/personagem.py
+++ aula2/personagem.py
@@ -1,1 +1,53 @@
+class Personagem:
+    def __init__(self):
+        self.nome = ""
+        self.vida = 100
+        self.forca = 10
 
+    def receber_dano(self, dano):
+        self.vida = self.vida - dano
+        if self.vida < 0:
+            self.vida = 0
+
+    def esta_vivo(self):
+        return self.vida > 0
+
+    def ficha(self):
+        print("Nome:", self.nome, "| Vida:", self.vida, "| Força:", self.forca)
+
+    def atacar(self, alvo):
+        print(self.nome, "ataca", alvo.nome)
+        alvo.receber_dano(self.forca)
+
+
+if __name__ == '__main__':
+    heroi = Personagem()
+    heroi.nome = "Heroi Link"
+    heroi.vida = 100
+    heroi.forca = 20
+
+    chefe = Personagem()
+    chefe.nome = "Skull Kid"
+    chefe.vida = 120
+    chefe.forca = 15
+
+    print("--- FICHA INICIAL ---")
+    heroi.ficha()
+    chefe.ficha()
+
+    while heroi.esta_vivo() and chefe.esta_vivo():
+        heroi.atacar(chefe)
+        if not chefe.esta_vivo():
+            print(chefe.nome, "morreu!")
+            break
+            
+        chefe.atacar(heroi)
+        if not heroi.esta_vivo():
+            print(heroi.nome, "morreu!")
+            break
+            
+        print("--------------------")
+
+    print("\n--- FICHA FINAL ---")
+    heroi.ficha()
+    chefe.ficha()
